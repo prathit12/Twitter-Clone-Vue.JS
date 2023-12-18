c9ef7748588a60d687b8486fc6f2343d72467353
prathit12 <nmb2509@gmail.com>
2023-12-18 14:10:51 -0700

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './/assets/css/reset.css'
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+
+createApp(App).use(store).use(router).mount('#app')
